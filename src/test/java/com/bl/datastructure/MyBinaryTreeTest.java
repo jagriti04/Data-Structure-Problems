d16f9410d5e74932f7509f2368945611ee1034ae
Jagriti <jagriti.agni@gmail.com>
2020-10-13 01:41:56 +0530

    [Jagriti]ADDED: test case for BST with many elements size check

--- src/test/java/com/bl/datastructure/MyBinaryTreeTest.java
+++ src/test/java/com/bl/datastructure/MyBinaryTreeTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MyBinaryTreeTest {
-	
+
 	// add elements in BST using binary tree node and calculating size of BST
 	@Test
 	public void givenThreeNumbers_whenAddedToBST_shouldReturnSizeThree() {
@@ -14,5 +14,17 @@ public class MyBinaryTreeTest {
 		myBinaryTree.add(70);
 		int size = myBinaryTree.getSize();
 		Assert.assertEquals(3, size);
+	}
+
+	// add elements in BST using binary tree node and calculating size of BST
+	@Test
+	public void givenThirteenNumbers_whenAddedToBST_shouldReturnSizeThirteen() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		Integer[] elements = {56, 30, 70, 22, 40, 60, 95, 11, 65, 3, 16, 63, 67};
+		for(Integer element: elements) {
+			myBinaryTree.add(element);
+		}
+		int size = myBinaryTree.getSize();
+		Assert.assertEquals(13, size);
 	}
 }
